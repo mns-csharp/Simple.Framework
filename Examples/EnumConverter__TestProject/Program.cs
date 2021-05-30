fdbe32163106142a10c6e3bf4c043e40b444d7f0
pc <pc@EDURAZEE>
2021-05-30 22:00:46 +0100

    FilePathVerifier.cs added

---
+++ Examples/EnumConverter__TestProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using Simple.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumConverter__TestProject
+{
+    class Program
+    {
+        private enum MyEnum{YES, NO }
+
+        static void Main(string[] args)
+        {
+            MyEnum myEnum = new MyEnum();
+            myEnum = MyEnum.NO;
+
+            int output = EnumConverter<MyEnum>.ToInt32(myEnum);
+
+            Console.WriteLine("Value = {0}", output);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Simple.Framework/Simple.Framework/Utils/FilePathVerifier.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Simple.Framework
+{
+    public class FilePathVerifier
+    {
+        public static bool IsValidPath(string path, bool allowRelativePaths = false)
+        {
+            bool isValid = true;
+
+            try
+            {
+                string fullPath = Path.GetFullPath(path);
+
+                if (allowRelativePaths)
+                {
+                    isValid = Path.IsPathRooted(path);
+                }
+                else
+                {
+                    string root = Path.GetPathRoot(path);
+                    isValid = string.IsNullOrEmpty(root.Trim(new char[] { '\\', '/' })) == false;
+                }
+            }
+            catch (Exception ex)
+            {
+                isValid = false;
+            }
+
+            return isValid;
+        }
+    }
+}
